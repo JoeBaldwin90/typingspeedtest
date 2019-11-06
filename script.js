5c002914148fad5be856635916b6e4ecb343a8d1
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-11-06 15:07:09 +0000

    Obey ESLint rules

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Elements
-const testAreaWrapper = document.querySelector(".test-area-wrapper");
-const testArea = document.querySelector("#user-typing-field");
-const startText = document.querySelector("#origin-text p");
-const resetButton = document.querySelector("#reset");
-const stopwatch = document.querySelector(".timer");
-const totalWordCounter = document.querySelector(".words");
-const finalWpmScore = document.querySelector(".wpm");
-const errorCount = document.querySelector(".errors");
-const difficultySelectors = document.querySelectorAll(".filters-list li a");
-const previousScore = document.querySelector(".previous-score");
+const testAreaWrapper = document.querySelector('.test-area-wrapper');
+const testArea = document.querySelector('#user-typing-field');
+const startText = document.querySelector('#origin-text p');
+const resetButton = document.querySelector('#reset');
+const stopwatch = document.querySelector('.timer');
+const totalWordCounter = document.querySelector('.words');
+const finalWpmScore = document.querySelector('.wpm');
+const errorCount = document.querySelector('.errors');
+const difficultySelectors = document.querySelectorAll('.filters-list li a');
+const previousScore = document.querySelector('.previous-score');
 
 // Initial state
-var timer = [0, 0, 0, 0];
-var interval;
-var timerRunning = false;
-var totalWordsTyped = 0;
-var wordSpeed = 0;
-var errors = 0;
+let timer = [0, 0, 0, 0];
+let interval;
+let timerRunning = false;
+let totalWordsTyped = 0;
+let wordSpeed = 0;
+let errors = 0;
 const textArray = [textEasy, textIntermediate, textExpert]; // defined in testText.js
 
 // ---------------------------- //
@@ -27,9 +27,9 @@ startText.innerHTML = textArray[0];
 // -------------------------------------------------------- //
 // Helper function: Add leading zero to numbers 9 or below //
 // ------------------------------------------------------ //
-const leadingZero = time => {
+const leadingZero = (time) => {
   if (time <= 9) {
-    time = "0" + time;
+    time = `0${time}`;
   }
   return time;
 };
@@ -38,11 +38,7 @@ const leadingZero = time => {
 // Run the stopwatch (minute : second : millisecond) //
 // ------------------------------------------------ //
 const runTimer = () => {
-  let currentTime = `
-  ${leadingZero(timer[0])}:
-  ${leadingZero(timer[1])}:
-  ${leadingZero(timer[2])}
-  `;
+  const currentTime = `${leadingZero(timer[0])}:${leadingZero(timer[1])}:${leadingZero(timer[2])}`;
   stopwatch.innerHTML = currentTime;
 
   // Increments by 1 every 10ms
@@ -52,22 +48,31 @@ const runTimer = () => {
   timer[2] = Math.floor(timer[3] - timer[1] * 100 - timer[0] * 6000);
 };
 
+// ---------------- //
+// Set final score //
+// -------------- //
+const setFinalScore = () => {
+  const wpmCalculation = totalWordsTyped / (timer[3] / 6000);
+  wordSpeed = wpmCalculation.toFixed(2);
+  finalWpmScore.innerText = `Score: ${wpmCalculation.toFixed(2)}wpm`;
+};
+
 // ----------------------------------------------------------- //
 // Match the text entered with the provided text on the page: //
 // --------------------------------------------------------- //
 const spellCheck = () => {
-  let sampleText = document.querySelector("#origin-text p").innerHTML;
-  let userEnteredText = testArea.value;
-  let sameTextAsShown = sampleText.substring(0, userEnteredText.length);
+  const sampleText = document.querySelector('#origin-text p').innerHTML;
+  const userEnteredText = testArea.value;
+  const sameTextAsShown = sampleText.substring(0, userEnteredText.length);
 
-  if (userEnteredText == sampleText) {
-    testAreaWrapper.style.borderColor = "#429890"; // Green
+  if (userEnteredText === sampleText) {
+    testAreaWrapper.style.borderColor = '#429890'; // Green
     clearInterval(interval);
     setFinalScore();
-  } else if (userEnteredText == sameTextAsShown) {
-    testAreaWrapper.style.borderColor = "#65CCF3"; // Blue
+  } else if (userEnteredText === sameTextAsShown) {
+    testAreaWrapper.style.borderColor = '#65CCF3'; // Blue
   } else {
-    testAreaWrapper.style.borderColor = "#E95D0F"; // Orange
+    testAreaWrapper.style.borderColor = '#E95D0F'; // Orange
   }
 };
 
@@ -75,28 +80,19 @@ const spellCheck = () => {
 // Increment number of words typed //
 // ------------------------------ //
 const calculateWords = () => {
-  let wordsTyped = testArea.value.split(" ").length - 1;
+  const wordsTyped = testArea.value.split(' ').length - 1;
   // Set global totalWordsTyped variable for wpm calculation
   totalWordsTyped = wordsTyped;
   totalWordCounter.innerText = `Words: ${wordsTyped}`;
 };
 
-// ---------------- //
-// Set final score //
-// -------------- //
-const setFinalScore = () => {
-  let wpmCalculation = totalWordsTyped / (timer[3] / 6000); 
-  wordSpeed = wpmCalculation.toFixed(2); 
-  finalWpmScore.innerText = `Score: ${wpmCalculation.toFixed(2)}wpm`;
-}
-
 // ------------- //
 // Count errors //
 // ----------- //
 const countErrors = () => {
-  let sampleText = document.querySelector("#origin-text p").innerHTML;
-  let userEnteredText = testArea.value;
-  let sameTextAsShown = sampleText.substring(0, userEnteredText.length);
+  const sampleText = document.querySelector('#origin-text p').innerHTML;
+  const userEnteredText = testArea.value;
+  const sameTextAsShown = sampleText.substring(0, userEnteredText.length);
 
   if (userEnteredText !== sameTextAsShown) {
     errors++;
@@ -108,7 +104,7 @@ const countErrors = () => {
 // Start the timer if text area is empty //
 // -------------------------------------//
 const startTimer = () => {
-  let userEnteredText = testArea.value.length;
+  const userEnteredText = testArea.value.length;
   if (userEnteredText === 0 && !timerRunning) {
     timerRunning = true;
     interval = setInterval(runTimer, 10);
@@ -118,18 +114,22 @@ const startTimer = () => {
 // ------------------------------------------------------ //
 // Change typing challenge copy and update selected link //
 // ---------------------------------------------------- //
-difficultySelectors.forEach(link => {
-  link.addEventListener("click", function(e) {
+difficultySelectors.forEach((link) => {
+  link.addEventListener('click', (e) => {
     e.preventDefault();
-    let level = this.getAttribute("difficulty-level");
+    let level = link.getAttribute('difficulty-level');
     let n = 0;
-    level == "easy"
-      ? (n = 0)
-      : level == "intermediate"
-      ? (n = 1)
-      : level == "hard"
-      ? (n = 2)
-      : (level = 0);
+
+    if (level === 'easy') {
+      n = 0;
+    } else if (level === 'intermediate') {
+      n = 1;
+    } else if (level === 'hard') {
+      n = 2;
+    } else {
+      level = 0;
+    }
+
     startText.innerHTML = textArray[n];
   });
 });
@@ -137,13 +137,13 @@ difficultySelectors.forEach(link => {
 // ------------------------------ //
 // Set classlist for clicked tab //
 // ---------------------------- //
-difficultySelectors.forEach(link => {
-  link.addEventListener("click", function(e) {
+difficultySelectors.forEach((link) => {
+  link.addEventListener('click', (e) => {
     e.preventDefault();
-    difficultySelectors.forEach(selector => {
-      selector.classList.remove("selected");
+    difficultySelectors.forEach((selector) => {
+      selector.classList.remove('selected');
     });
-    this.classList.add("selected");
+    link.classList.add('selected');
   });
 });
 
@@ -166,22 +166,22 @@ const reset = () => {
   timer = [0, 0, 0, 0];
   // Errors
   errors = 0;
-  errorCount.innerHTML = "Errors: 0";
+  errorCount.innerHTML = 'Errors: 0';
   // Word count
   totalWordsTyped = 0;
-  finalWpmScore.innerText = "Score: 0.00wpm";
+  finalWpmScore.innerText = 'Score: 0.00wpm';
   // Timer
-  testArea.value = "";
-  stopwatch.innerHTML = "00:00:00";
-  testAreaWrapper.style.borderColor = "gray";
+  testArea.value = '';
+  stopwatch.innerHTML = '00:00:00';
+  testAreaWrapper.style.borderColor = 'gray';
 };
 
 // --------------------------------------------------------- //
 // Event listeners for keyboard input and the reset button  //
 // ------------------------------------------------------- //
-testArea.addEventListener("keypress", startTimer, false);
-testArea.addEventListener("keyup", spellCheck, false);
-testArea.addEventListener("keyup", countErrors, false);
-testArea.addEventListener("keyup", calculateWords, false);
-resetButton.addEventListener("click", reset, false);
-resetButton.addEventListener("click", setPreviousScore, false);
+testArea.addEventListener('keypress', startTimer, false);
+testArea.addEventListener('keyup', spellCheck, false);
+testArea.addEventListener('keyup', countErrors, false);
+testArea.addEventListener('keyup', calculateWords, false);
+resetButton.addEventListener('click', reset, false);
+resetButton.addEventListener('click', setPreviousScore, false);
--- testText.js
+++ testText.js
@@ -1,8 +1,4 @@
-const textStart =
-  'Mary Jane and Peter Piper flew over the sea in a hot air balloon. "Wow!" they both cried.';
-const textEasy =
-  "Shakespeare was born and raised in Stratford-upon-Avon, Warwickshire. At the age of 18, he married Anne Hathaway, with whom he had three children.";
-const textIntermediate =
-  "A volcano is a rupture in the crust of a planetary-mass object, such as Earth, that allows hot lava, volcanic ash, and gases to escape from a magma chamber below the surface.";
-const textExpert =
-  "Boletus Edilus grows in deciduous and coniferous forests and tree plantations, forming symbiotic ectomycorrhizal associations with living trees by enveloping the tree's underground roots with sheaths of fungal tissue.";
+const textStart = 'Mary Jane and Peter Piper flew over the sea in a hot air balloon. "Wow!" they both cried.';
+const textEasy = 'Shakespeare was born and raised in Stratford-upon-Avon, Warwickshire. At the age of 18, he married Anne Hathaway, with whom he had three children.';
+const textIntermediate = 'A volcano is a rupture in the crust of a planetary-mass object, such as Earth, that allows hot lava, volcanic ash, and gases to escape from a magma chamber below the surface.';
+const textExpert = "Boletus Edilus grows in deciduous and coniferous forests and tree plantations, forming symbiotic ectomycorrhizal associations with living trees by enveloping the tree's underground roots with sheaths of fungal tissue.";
